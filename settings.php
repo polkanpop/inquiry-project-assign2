444c670e6b3f2e665d92622c87034cba1ae3eeee
Polkanpop <kanniepop@gmail.com>
2024-11-19 23:37:26 +0700

    changed the settings.php and added others

--- settings.php
+++ settings.php
@@ -3,13 +3,4 @@
     $user ="s105241532";
     $pass ="060305";
     $sql_db = "s105241532_db";
-
-    $conn = mysqli_connect($host, $user, $pass, $sql_db) ;
-    if($conn){
-        echo"Connection successful";
-        mysqli_close($conn);
-    }
-    else{
-        echo"Connection failed";
-    }   
 ?>
